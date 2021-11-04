7c24e35cadcf54088d2d0370880aa2cb29e50792
SichangiFelix <sichangifelix23@gmail.com>
2021-11-04 20:03:30 +0300

    main

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        accentColor: Colors.purple,
+      ),
+      //home: InputPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => InputPage(),
+      },
+    );
+  }
+}
